df8a700475a7fa7e8a25485b452fc4e683d225c8
krishnavaranasi2 <krishnavaranasi2@gmail.com>
2026-07-26 09:53:39 +0530

    added new lines of code

--- sample.py
+++ sample.py
@@ -6,3 +6,5 @@ from py_compile import main
 name = input("What is your name? ")
 
 print(f"Hello, {name}! Welcome to Python programming.")
+print("Shiva has initialized the git repository.")
+print("Shiva has made a change to the file.")   
